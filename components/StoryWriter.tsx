c4b5030ab0180fb90e63b8631ba6c689e3054c0d
Motar-Dev <mohammed.motar10@gmail.com>
2026-07-26 12:34:54 +0200

    style the terminal

--- components/StoryWriter.tsx
+++ components/StoryWriter.tsx
@@ -14,9 +14,12 @@ import { Textarea } from './ui/textarea';
 function StoryWriter() {
   const [story, setStory] = useState<string>('');
   const [pages, setPages] = useState<number>();
+  const [progress, setProgress] = useState();
+  const [runStarted, setRunStarted] = useState<boolean>(false);
+  const [runFinished, setRunFinished] = useState<boolean | null>(null);
 
   return (
-    <div className='flex flex-col container'>
+    <div className='flex flex-col container px-7'>
       <section className='flex-1 flex flex-col border border-purple-300 rounded-md p-10 space-y-2'>
         <Textarea
           value={story}
@@ -30,7 +33,7 @@ function StoryWriter() {
             <SelectValue placeholder='How many pages should the story be?' />
           </SelectTrigger>
 
-          <SelectContent className='w-full'>
+          <SelectContent>
             {Array.from({ length: 10 }, (_, i) => (
               <SelectItem key={i} value={String(i + 1)}>
                 {i + 1}
@@ -39,12 +42,27 @@ function StoryWriter() {
           </SelectContent>
         </Select>
 
-        <Button disabled={!story || !pages} className='w-full'>
+        <Button disabled={!story || !pages} className='w-full' size='lg'>
           Generate Story
         </Button>
       </section>
 
-      <section className='flex-1 pb-5 mt-5'></section>
+      <section className='flex-1 pb-5 mt-5'>
+        <div className='flex flex-col-reverse w-full space-y-2 bg-gray-800 rounded-md text-gray-200 font-mono p-10 h-96 overflow-y-auto'>
+          <div>
+            {runFinished === null && (
+              <>
+                <p className='animate-pulse mr-5'>
+                  Im waiting for you to Generate a story above...
+                </p>
+              </>
+            )}
+
+            <span className='mr-5'>{'>>'}</span>
+            {progress}
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
